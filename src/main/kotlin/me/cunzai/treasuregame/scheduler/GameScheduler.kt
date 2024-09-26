6d20a114a9887498f0fa905b886d1f1f9420813b
EmptyIrony <the_jerry_zhou@qq.com>
2024-09-26 23:13:16 +0800

    update

--- src/main/kotlin/me/cunzai/treasuregame/scheduler/GameScheduler.kt
+++ src/main/kotlin/me/cunzai/treasuregame/scheduler/GameScheduler.kt
@@ -102,7 +102,7 @@ object GameScheduler {
         val splitNumber = chestType.splitNumber
         if (splitNumber > 0) {
             val lastIndex = list.lastIndexOf(chestType)
-            if (lastIndex != -1 && lastIndex < splitNumber) {
+            if (lastIndex != -1 && lastIndex + 1 <= splitNumber) {
                 randomChest(list)
                 return
             }
